9fd02c4355b91ed88ff95f89d898ed45ff7f2a05
Alpha Olomi <alphaolomi@gmail.com>
2024-07-13 21:23:21 +0300

    feat: add Laravel backup
    - needs remote storage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,14 +1,15 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -21,6 +22,17 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        //
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30')
+            ->onFailure(function () {
+                // ...
+            })
+            ->onSuccess(function () {
+                // ...
+            });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
