9c759a235e119d5b9ef60edae7147e9ef56fcdc2
Alpha Olomi <alphaolomi@gmail.com>
2024-07-24 19:17:29 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,7 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        $count = 0;
+        // User::factory()->count(100_0_000)->create();
+        while ($count <= 100_000_000) {
+            $data = [
+                'name' => fake()->name(),
+                'email' => fake()->unique()->safeEmail(),
+                'email_verified_at' => [now(), null][random_int(0, 1)],
+                'password' =>  Hash::make('password'),
+                'remember_token' => [Str::random(10), null][random_int(0, 1)],
+                'welcome_valid_until' => null,
+            ];
+
+            DB::table('users')->insert($data);
+            $count++;
+
+            echo ".";
+            if (($count % 2500) == 0) echo "\n";
+        }
 
         try {
             User::factory()->create([
