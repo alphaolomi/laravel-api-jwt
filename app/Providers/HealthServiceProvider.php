2577502f4a848dd44e7854b33d13f2c767963b8f
Alpha Olomi <alphaolomi@gmail.com>
2024-07-24 18:25:34 +0300

    feat: add health checking

---
+++ app/Providers/HealthServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Spatie\Health\Facades\Health;
+// use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+
+
+use Spatie\Health\Checks\Checks\BackupsCheck;
+use Spatie\Health\Checks\Checks\DatabaseTableSizeCheck;
+use Spatie\Health\Checks\Checks\MeiliSearchCheck;
+use Spatie\Health\Checks\Checks\RedisMemoryUsageCheck;
+
+use Spatie\Health\Checks\Checks\CacheCheck;
+use Spatie\Health\Checks\Checks\DebugModeCheck;
+use Spatie\Health\Checks\Checks\OptimizedAppCheck;
+use Spatie\Health\Checks\Checks\ScheduleCheck;
+
+use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\EnvironmentCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Checks\Checks\UsedDiskSpaceCheck;
+
+use Spatie\Health\Checks\Checks\DatabaseConnectionCountCheck;
+use Spatie\Health\Checks\Checks\FlareErrorOccurrenceCountCheck;
+use Spatie\Health\Checks\Checks\QueueCheck;
+
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
+use Spatie\Health\Checks\Checks\HorizonCheck;
+use Spatie\Health\Checks\Checks\RedisCheck;
+
+
+class HealthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+
+
+        Health::checks([
+
+            BackupsCheck::new()->locatedAt(globPath:storage_path(path:config('backup.backup.name'))),
+            DatabaseTableSizeCheck::new(),
+            // MeiliSearchCheck::new(),
+            // RedisMemoryUsageCheck::new(),
+
+            CacheCheck::new(),
+            DebugModeCheck::new(),
+            OptimizedAppCheck::new(),
+            ScheduleCheck::new(),
+
+            DatabaseCheck::new(),
+            EnvironmentCheck::new(),
+            PingCheck::new()->url(config('app.url')),
+            UsedDiskSpaceCheck::new(),
+
+            DatabaseConnectionCountCheck::new(),
+            // FlareErrorOccurrenceCountCheck::new(),
+            QueueCheck::new(),
+
+            DatabaseSizeCheck::new(),
+            // HorizonCheck::new(),
+            // RedisCheck::new(),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Spatie\WelcomeNotification\WelcomesNewUsers;
 use App\Http\Controllers\MyWelcomeController;
-
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
+use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
 
 Route::get('/', function () {
     response()->noContent();
@@ -13,3 +14,7 @@ Route::group(['middleware' => ['web', WelcomesNewUsers::class,]], function () {
     Route::get('welcome/{user}', [MyWelcomeController::class, 'showWelcomeForm'])->name('welcome');
     Route::post('welcome/{user}', [MyWelcomeController::class, 'savePassword']);
 });
+
+
+Route::get('healthy', HealthCheckResultsController::class)->name('hwelcome');
+Route::get('healthj', HealthCheckJsonResultsController::class)->name('jwelcome');
