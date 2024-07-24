e7431f84cbc303419a7a7552113382c5170b573e
Alpha Olomi <alphaolomi@gmail.com>
2024-07-24 18:24:32 +0300

    Create UserTest.php

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Models\User;
+
+test('models can be instantiated', function () {
+    $user = User::factory()->create();
+
+    $this->expectsDatabaseQueryCount(2);
+    $this->assertModelExists($user);
+    $this->assertDatabaseCount('users', 1);
+
+
+
+});
