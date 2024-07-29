dcf834b461f777c39638234984959282201edc91
Alpha Olomi <alphaolomi@gmail.com>
2024-07-29 20:12:23 +0300

    Add some dummy logics

--- app/Console/Commands/SendMessage.php
+++ app/Console/Commands/SendMessage.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\RateLimiter;
 use stdClass;
+use function Laravel\Prompts\{text, textarea, confirm, password};
 
 class SendMessage extends Command
 {
@@ -13,7 +14,36 @@ class SendMessage extends Command
 
     protected $description = 'Command description';
 
-    public function handle() {}
+    public function handle()
+    {
+        $name = text(
+            label: 'What is your name?',
+            placeholder: 'E.g. Taylor Otwell',
+            required: true,
+            validate: fn (string $value) => match (true) {
+                strlen($value) < 3 => 'The name must be at least 3 characters.',
+                strlen($value) > 255 => 'The name must not exceed 255 characters.',
+                default => null
+            },
+            hint: 'This will be displayed on your profile.'
+        );
+
+        $password = password(
+            label: 'What is your password?',
+            required: 'The password is required.'
+        );
+
+        $confirmed = confirm(
+            label: 'Do you accept the terms?',
+            default: false,
+            yes: 'I accept',
+            no: 'I decline',
+            hint: 'The terms must be accepted to continue.'
+        );
+
+        $bio = textarea('Account Bio.');
+        $this->info($name);
+    }
 
     public function sayNiceJobFiveTimesPerMinute()
     {
