098ee2e692fcfae6473e6b3987310ea0194db309
Alpha Olomi <alphaolomi@gmail.com>
2024-07-30 22:19:09 +0300

    add Additional testing Assertions

---
+++ app/Providers/AdditionalAssertionsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Tests\Traits\AdditionalAssertions;
+
+class AdditionalAssertionsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        if (!\Illuminate\Testing\TestResponse::hasMacro('assertJsonTypedStructure')) {
+            \Illuminate\Testing\TestResponse::macro('assertJsonTypedStructure', function (array $structure) {
+                AdditionalAssertions::assertArrayStructure($structure, $this->json());
+
+                return $this;
+            });
+        }
+    }
+}
+
+
