870ce9babdcd6e563dfbc8bd39276ab28175e458
Alpha Olomi <alphaolomi@gmail.com>
2024-09-11 16:49:53 +0300

    move scheduled tasks to console file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -23,25 +24,13 @@ return Application::configure(basePath: dirname(__DIR__))
 
         //
     })
-    ->withSchedule(function (Schedule $schedule) {
-        //
-        // $schedule->command('backup:clean')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes();
-
-        $schedule->call(function () {
-            // DB::table('recent_users')->delete();
-            logger('test schedule: ' . time());
-        })->timezone('Africa/Dar_es_salaam')->everySecond();
-        // $schedule->command('backup:clean')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes();
-        $schedule->command('backup:run')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes()
-            ->onFailure(function () {
-                // ...
-            })
-            ->onSuccess(function () {
-                // ...
-            });
-    })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // $exceptions->render(function (Exception $e, Request $request) {
+        //     if ($request->is('api/*')) {
+        //         $responseData = [];
+        //         return response()->json($responseData);
+        //     }
+        // });
 
         $exceptions->shouldRenderJsonWhen(function (Request $request, Exception $e) {
             if ($request->is('api/*')) {
--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,23 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// ...
+
+
+   //
+        // $schedule->command('backup:clean')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes();
+
+// $schedule->call(function () {
+//             // DB::table('recent_users')->delete();
+//             logger('test schedule: ' . time());
+//         })->timezone('Africa/Dar_es_salaam')->everySecond();
+//         // $schedule->command('backup:clean')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes();
+
+// $schedule->command('backup:run')->timezone('Africa/Dar_es_salaam')->everyTwoMinutes()
+//             ->onFailure(function () {
+//                 // ...
+//             })
+//             ->onSuccess(function () {
+//                 // ...
+//             });
